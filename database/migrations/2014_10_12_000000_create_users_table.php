ff055ddd0c54a50640148d3f87982f269e77769f
wonder95 <steve@smgaweb.com>
2021-11-13 21:26:57 -0800

    Add status column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->enum('type', ['suppression', 'support', 'admin']);
+            $table->boolean('status')->default(1);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
